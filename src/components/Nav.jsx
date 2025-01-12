cd043519e9dcdc859aa370f5ca102a1f5359d769
fatou fall <fatoufall95@gmail.com>
2025-01-12 12:47:11 -0500

    added functional navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Link } from "react-router";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div>
+        <Link to="/">Home</Link>
+        &nbsp; | &nbsp;
+        <Link to="/budget">Financial Plan</Link>
+        &nbsp; | &nbsp;
+        <Link to="/goals">Goals</Link>
+        &nbsp; | &nbsp;
+        <Link to="/habits">Habits</Link>
+        &nbsp; | &nbsp;
+        <Link to="/reset">Reset</Link>
+      </div>
+      <div>Sign Out</div>
+    </nav>
+  );
 }
 
 export default Nav;
